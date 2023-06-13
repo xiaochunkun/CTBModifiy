4b11154a05d264cfd0d4deb5cb76c5a8e2d79d2b
flrscn <tomato-puddin@outlook.com>
2023-06-13 14:02:18 +0800

    移除Azul接口uri的latest参数

--- CurseTheBeast/Api/Azul/AzulApiClient.cs
+++ CurseTheBeast/Api/Azul/AzulApiClient.cs
@@ -12,6 +12,6 @@ public class AzulApiClient : BaseApiClient
 
     public Task<ZuluPackage[]> GetZuluPackageAsync(string javaVersion, string os, CancellationToken ct = default)
     { 
-        return GetAsync<ZuluPackage[]>(new Uri($"https://api.azul.com/metadata/v1/zulu/packages/?java_version={javaVersion}&os={os}&arch=x64&archive_type=zip&java_package_type=jre&javafx_bundled=false&latest=false&release_status=ga&availability_types=CA&page=1&page_size=100"), ZuluPackage.ZuluPackageArrayContext.Default.ZuluPackageArray, ct);
+        return GetAsync<ZuluPackage[]>(new Uri($"https://api.azul.com/metadata/v1/zulu/packages/?java_version={javaVersion}&os={os}&arch=x64&archive_type=zip&java_package_type=jre&javafx_bundled=false&release_status=ga&availability_types=CA&page=1&page_size=100"), ZuluPackage.ZuluPackageArrayContext.Default.ZuluPackageArray, ct);
     }
 }
